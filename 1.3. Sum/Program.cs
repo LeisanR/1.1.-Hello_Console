98c5b17a5f0987cdb98e60ece7167bc8a17aa2e2
LeisanR <leisan_chik@mail.ru>
2022-08-09 22:59:46 +0300

    int

---
+++ 1.3. Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу сложения двух чисел!
+int numberA = new Random () .Next (1, 10);
+Console.WriteLine (numberA);
+int numberB = new Random () .Next (1, 10);
+Console.WriteLine (numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
+
+
